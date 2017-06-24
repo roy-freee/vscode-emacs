e9d216c6c8ccf9e4f15847126103ae0011f8afcb
Robert Kwan <Robert.Kwan@moneytech.com.au>
2017-06-24 17:04:04 +1000

    linting now used

--- src/editor.ts
+++ src/editor.ts
@@ -1,6 +1,6 @@
-import * as vscode from 'vscode';
-import * as sexp from './sexp';
-import {RegisterContent, RectangleContent, RegisterKind} from './registers';
+import * as vscode from "vscode";
+import {RectangleContent, RegisterContent, RegisterKind} from "./registers";
+import * as sexp from "./sexp";
 
 enum KeybindProgressMode {
     None,   // No current keybind is currently in progress
@@ -8,17 +8,30 @@ enum KeybindProgressMode {
     RModeS, // 'Save Region in register' keybinding [started by 'C-x+r+s'] is currently in progress
     RModeI, // 'Insert Register content into buffer' keybinding [started by 'C-x+r+i'] is currently in progress
     AMode,  // (FUTURE, TBD) Abbrev keybinding  [started by 'C-x+a'] is currently in progress
-    MacroRecordingMode  // (FUTURE, TBD) Emacs macro recording [started by 'Ctrl-x+('] is currently in progress
+    MacroRecordingMode,  // (FUTURE, TBD) Emacs macro recording [started by 'Ctrl-x+('] is currently in progress
 };
 
 export class Editor {
+    public static delete(range: vscode.Range = null): Thenable<boolean> {
+        if (range === null) {
+            const start = new vscode.Position(0, 0);
+            const doc = vscode.window.activeTextEditor.document;
+            const end = doc.lineAt(doc.lineCount - 1).range.end;
+
+            range = new vscode.Range(start, end);
+        }
+        return vscode.window.activeTextEditor.edit((editBuilder) => {
+            editBuilder.delete(range);
+        });
+    }
+
     private killRing: string;
     private isKillRepeated: boolean;
     private keybindProgressMode: KeybindProgressMode;
-    private registersStorage: { [key:string] : RegisterContent; };
+    private registersStorage: { [key: string]: RegisterContent; };
 
     constructor() {
-        this.killRing = '';
+        this.killRing = "";
         this.isKillRepeated = false;
         this.keybindProgressMode = KeybindProgressMode.None;
         this.registersStorage = {};
@@ -27,58 +40,59 @@ export class Editor {
         });
     }
 
-    setStatusBarMessage(text: string): vscode.Disposable {
+    public setStatusBarMessage(text: string): vscode.Disposable {
         return vscode.window.setStatusBarMessage(text, 1000);
     }
 
-    setStatusBarPermanentMessage(text: string): vscode.Disposable {
-        return vscode.window.setStatusBarMessage(text); 
+    public setStatusBarPermanentMessage(text: string): vscode.Disposable {
+        return vscode.window.setStatusBarMessage(text);
     }
 
-    getSelectionRange(): vscode.Range {
-        let selection = vscode.window.activeTextEditor.selection,
-            start = selection.start,
-            end = selection.end;
+    public getSelectionRange(): vscode.Range {
+        const selection = vscode.window.activeTextEditor.selection;
+        const start = selection.start;
+        const end = selection.end;
 
         return (start.character !== end.character || start.line !== end.line) ? new vscode.Range(start, end) : null;
     }
 
-    getSelection(): vscode.Selection {
+    public getSelection(): vscode.Selection {
         return vscode.window.activeTextEditor.selection;
     }
 
-    setSelection(start: vscode.Position, end: vscode.Position): void {
-        let editor = vscode.window.activeTextEditor;
+    public setSelection(start: vscode.Position, end: vscode.Position): void {
+        const editor = vscode.window.activeTextEditor;
 
         editor.selection = new vscode.Selection(start, end);
     }
 
-    getLineText(): void {
+    public getLineText(): void {
         const ed = vscode.window.activeTextEditor;
         const line = ed.document.getText();
         const cursorPos = ed.selection.active.character;
         const afterCursor = line.substr(cursorPos);
         const whatAmI = sexp.sExpressionOrAtom(afterCursor);
 
-        if(whatAmI === sexp.Expression.Atom){
+        if (whatAmI === sexp.Expression.Atom) {
             vscode.commands.executeCommand("cursorWordRight");
-        }else{
+        } else {
             vscode.commands.executeCommand("cursorWordLeft");
         }
     }
 
-    /** Behave like Emacs kill command
-    */
-    kill(): void {
-        let saveIsKillRepeated = this.isKillRepeated,
-            promises = [
-                vscode.commands.executeCommand("emacs.exitMarkMode"),
-                vscode.commands.executeCommand("cursorEndSelect")
-            ];
+    /*
+     * Behave like Emacs kill command
+     */
+    public kill(): void {
+        const saveIsKillRepeated = this.isKillRepeated;
+        const promises = [
+            vscode.commands.executeCommand("emacs.exitMarkMode"),
+            vscode.commands.executeCommand("cursorEndSelect"),
+        ];
 
         Promise.all(promises).then(() => {
-            let selection = this.getSelection(),
-                range = new vscode.Range(selection.start, selection.end);
+            const selection = this.getSelection();
+            const range = new vscode.Range(selection.start, selection.end);
 
             this.setSelection(range.start, range.start);
             this.isKillRepeated = saveIsKillRepeated;
@@ -90,37 +104,8 @@ export class Editor {
         });
     }
 
-    private killEndOfLine(saveIsKillRepeated: boolean, range: vscode.Range): void {
-        let doc = vscode.window.activeTextEditor.document,
-            eof = doc.lineAt(doc.lineCount - 1).range.end;
-
-        if (doc.lineCount && !range.end.isEqual(eof) &&
-            doc.lineAt(range.start.line).rangeIncludingLineBreak) {
-            this.isKillRepeated ? this.killRing += '\n' : this.killRing = '\n';
-            saveIsKillRepeated = true;
-        } else {
-            this.setStatusBarMessage("End of buffer");
-        }
-        vscode.commands.executeCommand("deleteRight").then(() => {
-            this.isKillRepeated = saveIsKillRepeated;
-        });
-    }
-
-    private killText(range: vscode.Range): void {
-        let text = vscode.window.activeTextEditor.document.getText(range),
-            promises = [
-                Editor.delete(range),
-                vscode.commands.executeCommand("emacs.exitMarkMode")
-            ];
-
-        this.isKillRepeated ? this.killRing += text : this.killRing = text;
-        Promise.all(promises).then(() => {
-            this.isKillRepeated = true;
-        });
-    }
-
-    copy(range: vscode.Range = null): boolean {
-        this.killRing = '';
+    public copy(range: vscode.Range = null): boolean {
+        this.killRing = "";
         if (range === null) {
             range = this.getSelectionRange();
             if (range === null) {
@@ -133,8 +118,8 @@ export class Editor {
         return this.killRing !== undefined;
     }
 
-    cut(): boolean {
-        let range: vscode.Range = this.getSelectionRange();
+    public cut(): boolean {
+        const range: vscode.Range = this.getSelectionRange();
 
         if (!this.copy(range)) {
             return false;
@@ -143,45 +128,28 @@ export class Editor {
         return true;
     }
 
-    yank(): boolean {
+    public yank(): boolean {
         if (this.killRing.length === 0) {
             return false;
         }
-        vscode.window.activeTextEditor.edit(editBuilder => {
+        vscode.window.activeTextEditor.edit((editBuilder) => {
             editBuilder.insert(this.getSelection().active, this.killRing);
         });
         this.isKillRepeated = false;
         return true;
     }
 
-    undo(): void {
+    public undo(): void {
         vscode.commands.executeCommand("undo");
     }
 
-    private getFirstBlankLine(range: vscode.Range): vscode.Range {
-        let doc = vscode.window.activeTextEditor.document;
-
-        if (range.start.line === 0) {
-            return range;
-        }
-        range = doc.lineAt(range.start.line - 1).range;
-        while (range.start.line > 0 && range.isEmpty) {
-            range = doc.lineAt(range.start.line - 1).range;
-        }
-        if (range.isEmpty) {
-            return range;
-        } else {
-            return doc.lineAt(range.start.line + 1).range;
-        }
-    }
-
-    deleteBlankLines(): void {
-        let selection = this.getSelection(),
-            anchor = selection.anchor,
-            doc = vscode.window.activeTextEditor.document,
-            range = doc.lineAt(selection.start.line).range,
-            promises = [],
-            nextLine: vscode.Position;
+    public deleteBlankLines(): void {
+        const doc = vscode.window.activeTextEditor.document;
+        const promises = [];
+        let selection = this.getSelection();
+        let anchor = selection.anchor;
+        let range = doc.lineAt(selection.start.line).range;
+        let nextLine: vscode.Position;
 
         if (range.isEmpty) {
             range = this.getFirstBlankLine(range);
@@ -202,77 +170,62 @@ export class Editor {
         });
     }
 
-    static delete(range: vscode.Range = null): Thenable<boolean> {
-        if (range === null) {
-            let start = new vscode.Position(0, 0),
-                doc = vscode.window.activeTextEditor.document,
-                end = doc.lineAt(doc.lineCount - 1).range.end;
-
-            range = new vscode.Range(start, end);
-        }
-        return vscode.window.activeTextEditor.edit(editBuilder => {
-            editBuilder.delete(range);
-        });
-    }
-
-    setRMode(): void {
+    public setRMode(): void {
         this.setStatusBarPermanentMessage("C-x r");
-        this.keybindProgressMode = KeybindProgressMode.RMode; 
+        this.keybindProgressMode = KeybindProgressMode.RMode;
         return;
     }
 
-    onType(text: string): void {
+    public onType(text: string): void {
         let fHandled = false;
-        switch(this.keybindProgressMode)
-        {
+        switch (this.keybindProgressMode) {
             case KeybindProgressMode.RMode:
-                switch (text)
-                {
+                switch (text) {
                     // Rectangles
-                    case 'r':
+                    case "r":
                         this.setStatusBarMessage("'C-x r r' (Copy rectangle to register) is not supported.");
                         this.keybindProgressMode = KeybindProgressMode.None;
                         fHandled = true;
                         break;
 
-                    case 'k':
+                    case "k":
                         this.setStatusBarMessage("'C-x r k' (Kill rectangle) is not supported.");
                         this.keybindProgressMode = KeybindProgressMode.None;
                         fHandled = true;
                         break;
 
-                    case 'y':
+                    case "y":
                         this.setStatusBarMessage("'C-x r y' (Yank rectangle) is not supported.");
                         this.keybindProgressMode = KeybindProgressMode.None;
                         fHandled = true;
                         break;
 
-                    case 'o':
+                    case "o":
                         this.setStatusBarMessage("'C-x r o' (Open rectangle) is not supported.");
                         this.keybindProgressMode = KeybindProgressMode.None;
                         fHandled = true;
                         break;
 
-                    case 'c':
+                    case "c":
                         this.setStatusBarMessage("'C-x r c' (Blank out rectangle) is not supported.");
                         this.keybindProgressMode = KeybindProgressMode.None;
                         fHandled = true;
                         break;
 
-                    case 't':
+                    case "t":
                         this.setStatusBarMessage("'C-x r t' (prefix each line with a string) is not supported.");
                         this.keybindProgressMode = KeybindProgressMode.None;
                         fHandled = true;
                         break;
 
                     // Registers
-                    case 's':
+                    case "s":
                         this.setStatusBarPermanentMessage("Copy to register:");
                         this.keybindProgressMode = KeybindProgressMode.RModeS;
                         fHandled = true;
                         break;
 
-                    case 'i':
+                    case "i":
                         this.setStatusBarPermanentMessage("Insert register:");
                         this.keybindProgressMode = KeybindProgressMode.RModeI;
                         fHandled = true;
@@ -308,42 +261,88 @@ export class Editor {
 
         if (!fHandled) {
             // default input handling: pass control to VSCode
-            vscode.commands.executeCommand('default:type', {
-                text: text
+            vscode.commands.executeCommand("default:type", {
+                text,
             });
         }
-        return;    
+        return;
     }
 
-    SaveTextToRegister(registerName: string): void {
-        if (null == registerName) {
+    public SaveTextToRegister(registerName: string): void {
+        if (null === registerName) {
             return;
         }
-        let range : vscode.Range = this.getSelectionRange();
+        const range: vscode.Range = this.getSelectionRange();
         if (range !== null) {
-            let selectedText = vscode.window.activeTextEditor.document.getText(range);
+            const selectedText = vscode.window.activeTextEditor.document.getText(range);
             if (null !== selectedText) {
                 this.registersStorage[registerName] = RegisterContent.fromRegion(selectedText);
             }
         }
         return;
     }
-    
-    RestoreTextFromRegister(registerName: string): void {
-        vscode.commands.executeCommand("emacs.exitMarkMode"); // emulate Emacs 
-        let obj : RegisterContent = this.registersStorage[registerName];
-        if (null == obj) {
+
+    public RestoreTextFromRegister(registerName: string): void {
+        vscode.commands.executeCommand("emacs.exitMarkMode"); // emulate Emacs
+        const obj: RegisterContent = this.registersStorage[registerName];
+        if (null === obj) {
             this.setStatusBarMessage("Register does not contain text.");
             return;
         }
         if (RegisterKind.KText === obj.getRegisterKind()) {
-            const content : string | vscode.Position | RectangleContent = obj.getRegisterContent();
-            if (typeof content === 'string') {
-                vscode.window.activeTextEditor.edit(editBuilder => {
+            const content: string | vscode.Position | RectangleContent = obj.getRegisterContent();
+            if (typeof content === "string") {
+                vscode.window.activeTextEditor.edit((editBuilder) => {
                     editBuilder.insert(this.getSelection().active, content);
                 });
             }
-        }  
+        }
         return;
+    }
+
+    private killEndOfLine(saveIsKillRepeated: boolean, range: vscode.Range): void {
+        const doc = vscode.window.activeTextEditor.document;
+        const eof = doc.lineAt(doc.lineCount - 1).range.end;
+
+        if (doc.lineCount && !range.end.isEqual(eof) &&
+            doc.lineAt(range.start.line).rangeIncludingLineBreak) {
+            this.isKillRepeated ? this.killRing += "\n" : this.killRing = "\n";
+            saveIsKillRepeated = true;
+        } else {
+            this.setStatusBarMessage("End of buffer");
+        }
+        vscode.commands.executeCommand("deleteRight").then(() => {
+            this.isKillRepeated = saveIsKillRepeated;
+        });
+    }
+
+    private killText(range: vscode.Range): void {
+        const text = vscode.window.activeTextEditor.document.getText(range);
+        const promises = [
+            Editor.delete(range),
+            vscode.commands.executeCommand("emacs.exitMarkMode"),
+        ];
+
+        this.isKillRepeated ? this.killRing += text : this.killRing = text;
+        Promise.all(promises).then(() => {
+            this.isKillRepeated = true;
+        });
+    }
+
+    private getFirstBlankLine(range: vscode.Range): vscode.Range {
+        const doc = vscode.window.activeTextEditor.document;
+
+        if (range.start.line === 0) {
+            return range;
+        }
+        range = doc.lineAt(range.start.line - 1).range;
+        while (range.start.line > 0 && range.isEmpty) {
+            range = doc.lineAt(range.start.line - 1).range;
+        }
+        if (range.isEmpty) {
+            return range;
+        } else {
+            return doc.lineAt(range.start.line + 1).range;
+        }
     }
 }
--- src/sexp.ts
+++ src/sexp.ts
@@ -2,14 +2,46 @@
 //   S-expressions were popularised by Lisp and
 //   represent a recursive data structure.
 
-//   In the usual parenthesized syntax of Lisp, 
+//   In the usual parenthesized syntax of Lisp,
 //   an s-expression is classically defined as:
 //     1. an atom, or
 //     2. an expression of the form (x y) where x and y are s-expressions.
 
 export enum Expression {
   SExpression,
-  Atom
+  Atom,
+}
+
+export function turnToSexp(text: string): string[][] {
+  const start = new RegExp("[\{\(\[\<]");
+  const end = new RegExp("[\}\)\]\>]");
+  const isString = new RegExp("[\"\']");
+  const isWhitespace = new RegExp("\s");
+
+  const sexp = [[]];
+  let word = "";
+  let inStr = false;
+
+  for (const char of text){
+    if (start.test(char) && !inStr) {
+      sexp.push([]);
+    } else if ((end.test(char) || isWhitespace.test(char)) && !inStr) {
+      const last = sexp.length - 1;
+      if (word.length > 0) {
+        sexp[last].push(word);
+        word = "";
+      }
+
+      if (end.test(char)) {
+        const temp = sexp.pop();
+        sexp[last].push(temp);
+      }
+    } else if (isString.test(char)) {
+      inStr = !inStr;
+    }
+  }
+
+  return sexp[0];
 }
 
 function isSexp(text: string): boolean {
@@ -24,33 +56,3 @@ function isAtom(text: string): boolean {
 export function sExpressionOrAtom(text: string): Expression {
   return isSexp(text) ? Expression.SExpression : Expression.Atom;
 }
-
-export function parseSexpr(text: string): any {
-  var t = this.match(/\s*("[^"]*"|\(|\)|"|[^\s()"]+)/g)
-  for (var o, c=0, i=t.length-1; i>=0; i--) {
-    var n, ti = t[i].trim()
-    if (ti == '"') return
-    else if (ti == '(') t[i]='[', c+=1
-    else if (ti == ')') t[i]=']', c-=1
-    else if ((n=+ti) == ti) t[i]=n
-    else t[i] = '\'' + ti.replace('\'', '\\\'') + '\''
-    if (i>0 && ti!=']' && t[i-1].trim()!='(' ) t.splice(i,0, ',')
-    if (!c) if (!o) o=true; else return
-  }
-  return c ? undefined : eval(t.join(''))
-}
-
-  // public parse(s: string): any {
-  //   var t = s.match(/\s*("[^"]*"|\(|\)|"|[^\s()"]+)/g)
-  //   for (var o, c=0, i=t.length-1; i>=0; i--) {
-  //     var n, ti = t[i].trim();
-  //     if (ti == '"') return;
-  //     else if (ti == '(') t[i]='[', c+=1
-  //     else if (ti == ')') t[i]=']', c-=1
-  //     else if ((n=+ti) == ti) t[i]=n
-  //     else t[i] = '\'' + ti.replace('\'', '\\\'') + '\''
-  //     if (i>0 && ti!=']' && t[i-1].trim()!='(' ) t.splice(i,0, ',')
-  //     if (!c) if (!o) o=true; else return
-  //   }
-  //   return c ? undefined : eval(t.join(''))
-  // } 
